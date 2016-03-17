11e784ef5c98086aca45b4500594a768dff51519
Mauricio Minella <mauriciominella@gmail.com>
2016-03-16 22:27:04 -0300

    Concern separation on voting component. Next: Notice that we added a ref for the Winner component. It's something

--- voting-client/webpack.config.js
+++ voting-client/webpack.config.js
@@ -1,16 +1,20 @@
 var webpack = require('webpack');
+var autoprefixer = require('autoprefixer');
 
 module.exports = {
   entry: [
     'webpack-dev-server/client?http://localhost:8080',
     'webpack/hot/only-dev-server',
-    './src/index.js'
+    './src/index.jsx'
   ],
   module: {
     loaders: [{
       test: /\.jsx?$/,
       exclude: /node_modules/,
       loader: 'react-hot!babel'
+    }, {
+      test: /\.css$/,
+      loader: 'style!css!postcss'
     }]
   },
   resolve: {
@@ -27,5 +31,8 @@ module.exports = {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin()
-  ]
+  ],
+  postcss: function () {
+    return [autoprefixer];
+  }
 };
